d85abd5d24c39d4abe93f7ac86b3f8512640a000
vincent-guizot <vincentguizot128@gmail.com>
2025-03-03 05:06:03 +0700

    1

---
+++ build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// type biasanya untuk 1 variable
+// interface biasanya untuk object
+let printID = 10;
+const Person = {
+    id: 1,
+    name: "Jack",
+    age: 17,
+    isGraduated: true,
+    speak: function (str) {
+        console.log(str);
+    }
+};
